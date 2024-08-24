9b9b17db8e02aabb18a51486efdcf9c130fafa2e
yapyap2 <wonwoo42@gmail.com>
2024-08-24 20:40:51 +0900

    build.gradle 추가

---
+++ OFZ-common/src/main/java/org/ofz/TestComponent.java
@@ -0,0 +1,12 @@
+package org.ofz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestComponent {
+
+    public void testMethod(){
+        System.out.println("hello from common module!");
+    }
+
+}
