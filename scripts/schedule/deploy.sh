cd20dcb3ec886eebfd2a31dd33b118d078b38a8f
yapyap2 <99272057+yapyap2@users.noreply.github.com>
2024-08-27 08:18:51 +0900

    Update deploy.sh

--- scripts/schedule/deploy.sh
+++ scripts/schedule/deploy.sh
@@ -11,5 +11,5 @@
 
  JAR_PATH="/home/ubuntu/OFZ-schedule/OFZ-schedule.jar"
  chmod +x $JAR_PATH
- nohup java -jar $JAR_PATH >> /home/ubuntu/OFZ-schedule/deploy.log 2>> /home/ubuntu/OFZ-schedule/deploy_err.log &
+ nohup java -jar $JAR_PATH --spring.profiles.active=prod  >> /home/ubuntu/OFZ-schedule/deploy.log 2>> /home/ubuntu/OFZ-schedule/deploy_err.log &
  echo "schedule deploy success"
