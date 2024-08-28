295a879119162f4373c517bae29b29abeedca4ec
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-29 00:12:35 +0900

    ✨ feat: User 엔티티 컬럼 설정 추가
    loginId 컬럼에 유니크 제약조건 추가

--- OFZ-payment/src/main/java/org/ofz/payment/entity/User.java
+++ OFZ-payment/src/main/java/org/ofz/payment/entity/User.java
@@ -12,7 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "login_id")
+    @Column(name = "login_id", unique = true)
     private String loginId;
 
     private String password;
