5a08a58f996de813bda4f38d380a39745300c410
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-28 13:32:14 +0900

    ✨ feat: 웹소켓 관련 예외 추가
    uri 유무 관련 예외 처리 추가

---
+++ OFZ-payment/src/main/java/org/ofz/payment/exception/websocket/InvalidUriException.java
@@ -0,0 +1,8 @@
+package org.ofz.payment.exception.websocket;
+
+public class InvalidUriException extends RuntimeException {
+
+    public InvalidUriException(String message) {
+        super(message);
+    }
+}
