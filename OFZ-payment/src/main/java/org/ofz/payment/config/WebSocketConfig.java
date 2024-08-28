b73191c0b41eda74a298186b021607b9fa6a8026
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-29 00:03:58 +0900

    ⚒️ chore: WebSocketConfig URI 수정
    /test -> /socket 으로 수정

--- OFZ-payment/src/main/java/org/ofz/payment/config/WebSocketConfig.java
+++ OFZ-payment/src/main/java/org/ofz/payment/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
-        registry.addHandler(webSocketHandler, "/test")
+        registry.addHandler(webSocketHandler, "/socket")
                 .setAllowedOrigins("*");
     }
 }
