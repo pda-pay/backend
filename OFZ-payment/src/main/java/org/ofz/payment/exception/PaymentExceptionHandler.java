370c25454e38c0d70961534449e584a97eb53e59
jody816 <81346079+jody816@users.noreply.github.com>
2024-08-29 11:42:29 +0900

    chore: ExceptionHandler 클래스명 변경
    Global -> Payment

--- OFZ-payment/src/main/java/org/ofz/payment/exception/PaymentExceptionHandler.java
+++ OFZ-payment/src/main/java/org/ofz/payment/exception/PaymentExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class PaymentExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorDTO> catchSocketIdNullException(SocketIdNullException e) {
@@ -129,6 +129,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorDTO> catchPaymentTokenExpiredException(PaymentTokenExpiredException e) {
         log.error("exception class: {}", e.getClass());
+
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .message(e.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorDTO> catchNonValidPaymentTokenException(NonValidPaymentTokenException e) {
 
         ErrorDTO errorDTO = ErrorDTO.builder()
                 .message(e.getMessage())
