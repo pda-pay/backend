04ba2dabbe923e325826990c39f889a7e13336d9
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-02 13:43:03 +0900

    feat: 결제 페이지 관련 응답 dto 추가
    금월 결제할 금액,
    한도,
    연동 계좌,
    최근 결제 내역 두 개까지

---
+++ OFZ-payment/src/main/java/org/ofz/repayment/dto/PaymentInfoResponse.java
@@ -0,0 +1,27 @@
+package org.ofz.repayment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class PaymentInfoResponse {
+
+    private int paymentAmount;
+    private int creditLimit;
+    private int accountDeposit;
+    List<PaymentHistoriesResponse.PaymentHistoryDTO> paymentHistories;
+
+    public PaymentInfoResponse() {}
+
+    public PaymentInfoResponse(int paymentAmount, int creditLimit, int accountDeposit, List<PaymentHistoriesResponse.PaymentHistoryDTO> paymentHistories) {
+        this.paymentAmount = paymentAmount;
+        this.creditLimit = creditLimit;
+        this.accountDeposit = accountDeposit;
+        this.paymentHistories = paymentHistories;
+    }
+}
