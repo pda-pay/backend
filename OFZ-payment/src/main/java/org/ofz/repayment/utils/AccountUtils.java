9a2143ac0c7005be4b350c8e9b0584b4854b3be4
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-02 13:32:16 +0900

    feat: payment 더미 서버와 통신할 utils 추가
    추후에 로직 변경 필요
    계좌 정보 가져오기,
    계좌 잔액 차감 api

---
+++ OFZ-payment/src/main/java/org/ofz/repayment/utils/AccountUtils.java
@@ -0,0 +1,104 @@
+package org.ofz.repayment.utils;
+
+import org.ofz.repayment.dto.CashRepaymentResponse;
+import org.ofz.repayment.dto.AccountResponse;
+import org.ofz.repayment.exception.webclient.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AccountUtils {
+
+    @Value("${partner.api.signup-stock-data}")
+    private String PARTNERS_URL;
+    private final WebClient webClient;
+
+    public AccountUtils(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public synchronized AccountResponse fetchPaymentAccount(Long accountId) {
+
+        System.out.println("PARTNERS_URL = " + PARTNERS_URL);
+
+        AccountResponse response = null;
+
+        Map<String, String> map = new HashMap<>();
+        map.put("accountNumber", "381-6598-15037");
+
+        try {
+
+            response = webClient.post()
+                    .uri(PARTNERS_URL + "/accounts/deposits")
+                    .bodyValue(map)
+                    .retrieve()
+                    .bodyToMono(AccountResponse.class).block();
+
+            if (response == null) {
+                throw new WebClientResponseNullPointerException("응답 받은 상환 계좌 정보 데이터가 없습니다.");
+            }
+
+        } catch (WebClientRequestException e) {
+            throw new WebClientBadRequestException("네트워크 연결 문제 또는 잘못된 URI 요청입니다.");
+
+        } catch (WebClientResponseException  e) {
+            if (e.getStatusCode().is4xxClientError()) {
+                throw new WebClientErrorException("요청 데이터를 확인해주세요.", e.getStatusCode());
+
+            } else if (e.getStatusCode().is5xxServerError()) {
+                throw new WebClientServerErrorException("서버 에러가 발생했습니다.", e.getStatusCode());
+            }
+
+        } catch (IllegalArgumentException e) {
+            throw new WebClientIllegalArgumentException("잘못된 인자를 메서드에 전달하였습니다.", e.getMessage());
+        }
+
+        response.setAccountNumber("어카운트 넘버");
+
+        return response;
+    }
+
+    public synchronized CashRepaymentResponse fetchCashRepayment(Long accountId, int value) {
+        CashRepaymentResponse response = null;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("accountNumber", "381-6598-15037");
+        map.put("value", value);
+
+        try {
+
+            response = webClient.put()
+                    .uri(PARTNERS_URL + "/accounts/withdraw")
+                    .bodyValue(map)
+                    .retrieve()
+                    .bodyToMono(CashRepaymentResponse.class).block();
+
+            if (response == null) {
+                throw new WebClientResponseNullPointerException("응답 받은 상환 계좌 정보 데이터가 없습니다.");
+            }
+
+        } catch (WebClientRequestException e) {
+            throw new WebClientBadRequestException("네트워크 연결 문제 또는 잘못된 URI 요청입니다.");
+
+        } catch (WebClientResponseException  e) {
+            if (e.getStatusCode().is4xxClientError()) {
+                throw new WebClientErrorException("요청 데이터를 확인해주세요.", e.getStatusCode());
+
+            } else if (e.getStatusCode().is5xxServerError()) {
+                throw new WebClientServerErrorException("서버 에러가 발생했습니다.", e.getStatusCode());
+            }
+
+        } catch (IllegalArgumentException e) {
+            throw new WebClientIllegalArgumentException("잘못된 인자를 메서드에 전달하였습니다.", e.getMessage());
+        }
+
+        response.setRepaymentAmount(value);
+        return response;
+    }
+}
