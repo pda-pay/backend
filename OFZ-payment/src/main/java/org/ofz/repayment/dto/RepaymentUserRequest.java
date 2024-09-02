08dfe0fc17130364b921ecd35020a4de4839655f
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-02 13:12:48 +0900

    feat: 선결제 관련 메서드 수행을 위한 DTO 추가
    유저 아이디값만을 받음

---
+++ OFZ-payment/src/main/java/org/ofz/repayment/dto/RepaymentUserRequest.java
@@ -0,0 +1,13 @@
+package org.ofz.repayment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepaymentUserRequest {
+
+    private Long userId;
+}
