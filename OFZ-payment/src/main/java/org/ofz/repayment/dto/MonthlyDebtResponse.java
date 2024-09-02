6c5f4eb2116aa9be80cfdca3a9fc5d1481127053
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-02 13:32:58 +0900

    feat: 월별 결제할 금액 반환 dto 추가

---
+++ OFZ-payment/src/main/java/org/ofz/repayment/dto/MonthlyDebtResponse.java
@@ -0,0 +1,21 @@
+package org.ofz.repayment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+public class MonthlyDebtResponse {
+
+    private int repaymentDate;
+    private int previousMonthDebt;
+    private int currentMonthDebt;
+
+    public MonthlyDebtResponse(int repaymentDate, int previousMonthDebt, int currentMonthDebt) {
+        this.repaymentDate = repaymentDate;
+        this.previousMonthDebt = previousMonthDebt;
+        this.currentMonthDebt = currentMonthDebt;
+    }
+}
