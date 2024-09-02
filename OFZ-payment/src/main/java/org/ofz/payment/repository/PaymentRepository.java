7f5af3284ff7f245b1c0d7689ba0b42a27dc8905
YaRkyungmin <kkgm94@naver.com>
2024-09-02 13:13:08 +0900

    ✨ feat: 신용 한도 조회, 총 결제금액 조회 쿼리 메소드 생성

--- OFZ-payment/src/main/java/org/ofz/payment/repository/PaymentRepository.java
+++ OFZ-payment/src/main/java/org/ofz/payment/repository/PaymentRepository.java
@@ -2,10 +2,17 @@ package org.ofz.payment.repository;
 
 import org.ofz.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findPaymentByUserId(Long userId);
+    @Query("SELECT p.creditLimit FROM Payment p WHERE p.user.id = :userId")
+    Optional<Integer> findCreditLimitByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT COALESCE(SUM(p.previousMonthDebt + p.currentMonthDebt), 0) FROM Payment p WHERE p.user.id = :userId")
+    int findTotalDebtByUserId(@Param("userId") Long userId);
 }
