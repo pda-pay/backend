59534f17e9d7070a56d2a87b10b887e6738062ed
YaRkyungmin <kkgm94@naver.com>
2024-09-02 13:18:08 +0900

    ✨ feat: 결제서비스 가입 및 수정 관련 Exception 추가

---
+++ OFZ-payment/src/main/java/org/ofz/management/exception/FetchPriviousStockPriceException.java
@@ -0,0 +1,7 @@
+package org.ofz.management.exception;
+
+public class FetchPriviousStockPriceException extends RuntimeException {
+    public FetchPriviousStockPriceException(String message) {
+        super(message);
+    }
+}
