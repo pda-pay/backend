ab0ea046c64c215537d4aca78b2d127d48811ec2
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-02 13:13:30 +0900

    feat: 현금 선결제 응답 DTO 추가
    결제한 금액, 메시지를 반환

---
+++ OFZ-payment/src/main/java/org/ofz/repayment/dto/CashRepaymentResponse.java
@@ -0,0 +1,19 @@
+package org.ofz.repayment.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+public class CashRepaymentResponse {
+
+    private int repaymentAmount;
+    private String message;
+
+    public CashRepaymentResponse() {}
+
+    public CashRepaymentResponse(int repaymentAmount, String message) {
+        this.repaymentAmount = repaymentAmount;
+        this.message = message;
+    }
+}
