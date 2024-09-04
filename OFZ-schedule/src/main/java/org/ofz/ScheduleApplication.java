f8c982eaaafaccd3b8aebaff3c862de30e562443
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-04 19:17:42 +0900

    feat: 스케쥴 로직 추가

--- OFZ-schedule/src/main/java/org/ofz/ScheduleApplication.java
+++ OFZ-schedule/src/main/java/org/ofz/ScheduleApplication.java
@@ -2,10 +2,15 @@ package org.ofz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableAsync
+@EnableScheduling
 public class ScheduleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
