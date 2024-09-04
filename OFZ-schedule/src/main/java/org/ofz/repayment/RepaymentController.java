ea4ef3bbed4b2aceadee280e5c12b1ea683a9808
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-09-04 10:58:16 +0900

    refactor: 상환 로직 스케줄러 관련 api path 변경

--- OFZ-schedule/src/main/java/org/ofz/repayment/RepaymentController.java
+++ OFZ-schedule/src/main/java/org/ofz/repayment/RepaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/repayments")
+@RequestMapping("/schedule")
 public class RepaymentController {
 
     private final RepaymentService repaymentService;
