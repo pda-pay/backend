edaa9406d47f008c8772e2fa7ac17788c9678058
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-08 02:14:57 +0900

    feat: StockPriorityRepo 메서드 추가

--- OFZ-payment/src/main/java/org/ofz/management/repository/StockPriorityRepository.java
+++ OFZ-payment/src/main/java/org/ofz/management/repository/StockPriorityRepository.java
@@ -3,6 +3,7 @@ package org.ofz.management.repository;
 import org.ofz.management.StockPriority;
 import org.ofz.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +19,13 @@ public interface StockPriorityRepository extends JpaRepository<StockPriority, Lo
     );
 
     void deleteAllByUserId(Long userId);
+
+    boolean existsByUserId(Long userId);
+
+    List<StockPriority> findStockPrioritiesByUserIdOrderByStockRank(Long userId);
+
+    @Query("SELECT SUM(sp.quantity) AS sum FROM StockPriority sp " +
+            "WHERE sp.user.id = :userId AND sp.accountNumber = :accountNumber " +
+            "AND sp.stockCode = :stockCode")
+    Optional<Integer> findStockPriorityQuantityByUserIdAndAccountNumberAndStockCode(Long userId, String accountNumber, String stockCode);
 }
