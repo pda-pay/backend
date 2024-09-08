f61403de501b97268b637b9d1bcff381e35ae5c9
YaRkyungmin <kkgm94@naver.com>
2024-09-09 00:17:21 +0900

    ✨ feat: 알림 컨트롤러 로직 추가

--- OFZ-notification/src/main/java/org/ofz/controller/NotificationController.java
+++ OFZ-notification/src/main/java/org/ofz/controller/NotificationController.java
@@ -2,11 +2,13 @@ package org.ofz.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.ofz.service.NotificationService;
-import org.ofz.dto.SaveTokenRequest;
-import org.ofz.dto.SaveUserTokenResponse;
+import org.ofz.dto.api.SaveTokenRequest;
+import org.ofz.dto.api.TokenResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +16,23 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
     private final NotificationService notificationService;
     @PostMapping("/tokens")
-    public ResponseEntity<SaveUserTokenResponse> saveUserFCMToken(@RequestHeader("X-LOGIN-ID") String userId, @RequestBody SaveTokenRequest saveTokenRequest) {
-        SaveUserTokenResponse saveUserTokenResponse = notificationService.saveUserToken(userId, saveTokenRequest);
+    public ResponseEntity<TokenResponse> saveUserFCMToken(@RequestHeader("X-LOGIN-ID") String userId, @RequestBody SaveTokenRequest saveTokenRequest) {
+        TokenResponse tokenResponse = notificationService.saveUserToken(userId, saveTokenRequest);
 
-        return ResponseEntity.status(HttpStatus.OK).body(saveUserTokenResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(tokenResponse);
+    }
+
+    @PutMapping("/tokens")
+    public ResponseEntity<TokenResponse> deleteUserFCMToken(@RequestHeader("X-LOGIN-ID") String userId) {
+        TokenResponse tokenResponse = notificationService.deleteUserToken(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(tokenResponse);
+    }
+
+    @GetMapping("/tokens/test")
+    public ResponseEntity<?> test() throws IOException {
+        notificationService.test();
+
+        return ResponseEntity.status(HttpStatus.OK).body("good");
     }
 }
