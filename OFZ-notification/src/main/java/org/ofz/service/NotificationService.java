57f6af20bafb326c213905600ce6f3529406db0d
YaRkyungmin <kkgm94@naver.com>
2024-09-09 00:16:54 +0900

    ✨ feat: 알림 서비스 로직 추가

--- OFZ-notification/src/main/java/org/ofz/service/NotificationService.java
+++ OFZ-notification/src/main/java/org/ofz/service/NotificationService.java
@@ -1,19 +1,43 @@
 package org.ofz.service;
 
 import lombok.RequiredArgsConstructor;
-import org.ofz.dto.CacheTokenDto;
-import org.ofz.dto.SaveTokenRequest;
-import org.ofz.dto.SaveUserTokenResponse;
-import org.ofz.service.CacheTokenService;
+import org.ofz.dto.redis.CacheTokenDto;
+import org.ofz.dto.api.SaveTokenRequest;
+import org.ofz.dto.api.TokenResponse;
+import org.ofz.rabbitMQ.rabbitDto.Example;
+import org.ofz.rabbitMQ.rabbitDto.NotificationMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class NotificationService {
     private final CacheTokenService cacheTokenService;
-    public SaveUserTokenResponse saveUserToken(String userId, SaveTokenRequest saveTokenRequest) {
+    private final FCMService fcmService;
+    private final static String QUEUE_NAME = "notification";
+
+    public TokenResponse saveUserToken(String userId, SaveTokenRequest saveTokenRequest) {
         cacheTokenService.cacheToken(new CacheTokenDto(userId, saveTokenRequest.getToken()));
 
-        return SaveUserTokenResponse.sucess();
+        return TokenResponse.saveSuccess();
+    }
+
+    public TokenResponse deleteUserToken(String userId) {
+        cacheTokenService.deleteCachedToken(userId);
+
+        return TokenResponse.deleteSuccess();
+    }
+
+    public void test() throws IOException {
+        System.out.println(fcmService.getAccessToken());
+    }
+
+    @RabbitListener(queues = QUEUE_NAME)
+    public void receiveMessage(NotificationMessage notificationMessage) {
+        CacheTokenDto cacheTokenDto = cacheTokenService.getCachedToken(notificationMessage.getUserId());
+        fcmService.sendFcmMessageByNotificationMessage(notificationMessage, cacheTokenDto.getToken());
     }
 }
