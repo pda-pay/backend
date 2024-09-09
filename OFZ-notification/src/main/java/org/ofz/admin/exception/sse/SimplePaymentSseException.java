dd02f3641559f4e5dc8f9171cc70a8171f000875
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-09 09:31:28 +0900

    feat: 알람 모듈 간편 결제, 선상환 예외 처리 추가

---
+++ OFZ-notification/src/main/java/org/ofz/admin/exception/sse/SimplePaymentSseException.java
@@ -0,0 +1,8 @@
+package org.ofz.admin.exception.sse;
+
+public class SimplePaymentSseException extends RuntimeException {
+
+    public SimplePaymentSseException(String message) {
+        super(message);
+    }
+}
