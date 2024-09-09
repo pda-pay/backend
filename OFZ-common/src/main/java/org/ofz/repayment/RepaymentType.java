d5de248cb907ea776d40d77d8886ce65ecd4cab4
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-09 09:32:25 +0900

    feat: 상환 Enum 변경

--- OFZ-common/src/main/java/org/ofz/repayment/RepaymentType.java
+++ OFZ-common/src/main/java/org/ofz/repayment/RepaymentType.java
@@ -1,9 +1,15 @@
 package org.ofz.repayment;
 
 public enum RepaymentType {
-    CASH,
-    PAWN,
-    PRE_CASH,
-    PRE_PAWN,
-    OFFSET
+    CASH("현금"),
+    PAWN("담보"),
+    PRE_CASH("현금선상환"),
+    PRE_PAWN("담보선상환"),
+    OFFSET("반대매매");
+
+    public String kor;
+
+    RepaymentType(String kor) {
+        this.kor = kor;
+    }
 }
