4445aad5c172e8e0c9b5d712061872f4c7c0426e
wlsdk9803 <wlsdk9803@naver.com>
2024-09-09 19:49:46 +0900

    style: AllPayedOffsetLogDto 주석 삭제

--- OFZ-common/src/main/java/org/ofz/rabbitMQ/rabbitDto/AllPayedOffsetLogDto.java
+++ OFZ-common/src/main/java/org/ofz/rabbitMQ/rabbitDto/AllPayedOffsetLogDto.java
@@ -11,10 +11,6 @@ import org.ofz.rabbitMQ.Queueable;
 @NoArgsConstructor
 @Builder
 public class AllPayedOffsetLogDto implements Queueable {
-    // 해당 사용자 stock, mortgaged_stock, stock_priority 변경
-    // if(excessPayment > 0)해당 사용자 'mortgagedStockProjection.getAccountNumber()'의 잔고 excessPayment만큼 증가
-    // 해당 사용자의 payment 테이블 previous_month_debt 0으로, overdueDay null로, payFlag true으로 변경됨
-
     private Long id;
     private String loginId;
     private String title;
