9ebabdf51bb85df5c4c173b2b304f5ccbe27a0ce
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-09-09 14:25:38 +0900

    feat: asset 모듈 관련 dto 내 날짜 형식 삭제

--- OFZ-asset/src/main/java/org/ofz/asset/dto/AssetHistoryRateRes.java
+++ OFZ-asset/src/main/java/org/ofz/asset/dto/AssetHistoryRateRes.java
@@ -2,24 +2,19 @@ package org.ofz.asset.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 public class AssetHistoryRateRes {
 
     private Long id;
     private Long userId;
-    private String createdAt;
     private double mortgageSumRateOfChange;
 
-
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-    public AssetHistoryRateRes(Long id, Long userId, LocalDateTime createdAt, double mortgageSumRateOfChange) {
+    public AssetHistoryRateRes(Long id, Long userId, double mortgageSumRateOfChange) {
         this.id = id;
         this.userId = userId;
-        this.createdAt = createdAt.format(DATE_FORMATTER);
         this.mortgageSumRateOfChange = mortgageSumRateOfChange;
     }
 
