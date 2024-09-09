13a6dcc1a09125f3cb66def791487057872b4601
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-09-09 20:44:29 +0900

    feat: Asset History Entity에 created_at, updated_at 컬럼값이 자동으로 등록되도록 변경

--- OFZ-asset/src/main/java/org/ofz/asset/entity/AssetHistory.java
+++ OFZ-asset/src/main/java/org/ofz/asset/entity/AssetHistory.java
@@ -28,10 +28,10 @@ public class AssetHistory {
     @Column(name = "margin_requirement", nullable = false)
     private int marginRequirement;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @Column(name = "mortgage_sum_rate_of_change")
@@ -39,6 +39,19 @@ public class AssetHistory {
 
     @Column(name = "max_limit", nullable = false)
     private int maxLimit;
+
+    // 생성 시점에 createdAt 설정
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // 업데이트 시점에 updatedAt 설정
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     public void updateMortgageSumRateOfChange(double rateOfChange) {
         this.mortgageSumRateOfChange = rateOfChange;
