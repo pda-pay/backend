64960d327ad35f67b849ce5d669142011946fec6
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-09-09 20:41:33 +0900

    feat: 담보유지비율 엔티티에 최대 한도 컬럼 추가 및 created_at, updated_at 컬럼값이 자동으로 등록되도록 변경

--- OFZ-common/src/main/java/org/ofz/marginRequirement/entity/MarginRequirementHistory.java
+++ OFZ-common/src/main/java/org/ofz/marginRequirement/entity/MarginRequirementHistory.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "margin_requirement_history")
 public class MarginRequirementHistory {
 
@@ -24,19 +26,44 @@ public class MarginRequirementHistory {
     @Column(name = "today_limit", nullable = false)
     private int todayLimit;
 
+    @Column(name = "max_limit", nullable = false)
+    private int maxLimit;
+
     @Column(name = "margin_requirement", nullable = false)
     private int marginRequirement;
 
-    public MarginRequirementHistory(Long userId, int mortgageSum, int todayLimit, int marginRequirement) {
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    // 생성 시점에 createdAt 설정
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // 업데이트 시점에 updatedAt 설정
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+    public MarginRequirementHistory(Long userId, int mortgageSum, int todayLimit, int maxLimit, int marginRequirement) {
         this.userId = userId;
         this.mortgageSum = mortgageSum;
         this.todayLimit = todayLimit;
+        this.maxLimit = maxLimit;
         this.marginRequirement = marginRequirement;
     }
 
-    public void updateValues(int mortgageSum, int todayLimit, int marginRequirement) {
+    public void updateValues(int mortgageSum, int todayLimit, int maxLimit, int marginRequirement) {
         this.mortgageSum = mortgageSum;
         this.todayLimit = todayLimit;
+        this.maxLimit = maxLimit;
         this.marginRequirement = marginRequirement;
     }
 }
