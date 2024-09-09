d7f02466b9dd17a81f2ae19c0165c615527bd3cf
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-09-09 14:29:24 +0900

    fix: 불필요한 파일 삭제

--- OFZ-asset/src/main/java/org/ofz/config/AssetDataSourceConfig.java
+++ OFZ-asset/src/main/java/org/ofz/config/AssetDataSourceConfig.java
@@ -1,48 +1,52 @@
-package org.ofz.config;
-
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-
-@Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-        basePackages = "org.ofz.repository.asset",
-        entityManagerFactoryRef = "assetEntityManagerFactory",
-        transactionManagerRef = "assetTransactionManager"
-)
-public class AssetDataSourceConfig {
-
-    @Bean(name = "assetDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.asset")
-    public DataSource assetDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean(name = "assetEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean assetEntityManagerFactory(
-            EntityManagerFactoryBuilder builder, @Qualifier("assetDataSource") DataSource dataSource) {
-        return builder
-                .dataSource(dataSource)
-                .packages("com.example.entity.asset")
-                .persistenceUnit("asset")
-                .build();
-    }
-
-    @Bean(name = "assetTransactionManager")
-    public PlatformTransactionManager assetTransactionManager(
-            @Qualifier("assetEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-}
+//package org.ofz.config;
+//
+//import jakarta.persistence.EntityManagerFactory;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@EnableTransactionManagement
+//@EnableJpaRepositories(
+//        basePackages = "org.ofz.repository.asset",
+//        entityManagerFactoryRef = "assetEntityManagerFactory",
+//        transactionManagerRef = "assetTransactionManager"
+//)
+//public class AssetDataSourceConfig {
+//
+//    @Primary
+//    @Bean(name = "assetDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.asset")
+//    public DataSource assetDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "assetEntityManagerFactory")
+//    public LocalContainerEntityManagerFactoryBean assetEntityManagerFactory(
+//            EntityManagerFactoryBuilder builder, @Qualifier("assetDataSource") DataSource dataSource) {
+//        return builder
+//                .dataSource(dataSource)
+//                .packages("com.example.entity.asset")
+//                .persistenceUnit("asset")
+//                .build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "assetTransactionManager")
+//    public PlatformTransactionManager assetTransactionManager(
+//            @Qualifier("assetEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+//        return new JpaTransactionManager(entityManagerFactory);
+//    }
+//}
