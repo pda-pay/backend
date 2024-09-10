5b833aeea74ec767835d93e0f1abc3963ca25fb9
yapyap2 <wonwoo42@gmail.com>
2024-09-10 10:56:17 +0900

    hotfix: cookie 도메인 개발환경으로 변경

--- OFZ-payment/src/main/java/org/ofz/user/UserController.java
+++ OFZ-payment/src/main/java/org/ofz/user/UserController.java
@@ -54,7 +54,7 @@ public class UserController {
 
         ResponseCookie accessTokenCookie = ResponseCookie.from( "accessToken", jwtToken.getAccessToken())
                 .path("/")
-                .domain("140pay.site")
+//                .domain("140pay.site")
                 .maxAge(12 * 60 * 60)
                 .sameSite("None")
                 .secure(true)
