467a1361a639860efe8f228dc0fc787375ba9ad1
jody816 <81346079+jody816@users.noreply.github.com>
2024-09-10 11:39:01 +0900

    feat: 알람페이지 이넘 로직 추가

--- OFZ-common/src/main/java/org/ofz/rabbitMQ/rabbitDto/NotificationMessage.java
+++ OFZ-common/src/main/java/org/ofz/rabbitMQ/rabbitDto/NotificationMessage.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ofz.rabbitMQ.NotificationPage;
 import org.ofz.rabbitMQ.NotificationType;
 import org.ofz.rabbitMQ.Queueable;
 
@@ -15,13 +16,15 @@ public class NotificationMessage implements Queueable {
     private String title;
     private String body;
     private NotificationType category;
+    private NotificationPage page;
 
     @Builder
-    public NotificationMessage(String loginId, String title, String body, NotificationType category) {
+    public NotificationMessage(String loginId, String title, String body, NotificationType category, NotificationPage page) {
         this.loginId = loginId;
         this.title = title;
         this.body = body;
         this.category = category;
+        this.page = page;
     }
 
     @Override
